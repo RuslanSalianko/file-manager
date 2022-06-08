1dce1db5c604c8b26e13cd1a73643f912774ffbc
Ruslan Salianko <r.salianko@gmail.com>
2022-06-08 15:54:14 +0300

    fix: перевод на новую строку вприведственном соощении

--- message/welcome.mjs
+++ message/welcome.mjs
@@ -1,5 +1,5 @@
 function welcomeMessage(userName) {
-  process.stdout.write(`Welcome to the File Manager, ${userName}!`)
+  process.stdout.write(`Welcome to the File Manager, ${userName}!\n`)
 }
 
 export default welcomeMessage;

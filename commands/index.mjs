1ace890bd3c9b34092477e29660516311f22c98e
Ruslan Salianko <r.salianko@gmail.com>
2022-06-09 13:39:22 +0300

    feat: добавление команды up

--- commands/index.mjs
+++ commands/index.mjs
@@ -1,5 +1,6 @@
 import invalidMessage from "../message/invalid.mjs";
 import exit from "./exit.mjs";
+import up from "./navigation/up.mjs";
 import os from "./os/index.mjs";
 
 function run(command, data) {
@@ -9,6 +10,9 @@ function run(command, data) {
       break;
     case 'os':
       os(command[1])
+      break;
+    case 'up':
+      up();
       break;
     default:
       invalidMessage();
--- index.mjs
+++ index.mjs
@@ -1,3 +1,4 @@
+import { homedir } from 'os';
 import * as readline from 'readline';
 import run from "./commands/index.mjs";
 import exitMessage from "./message/exit.mjs";
@@ -7,17 +8,23 @@ function start() {
   const argv = process.argv.slice(2);
   const userName = argv[1].split('=')[1];
   welcomeMessage(userName);
-
+  process.env.currentDir = homedir();
+  process.stdout.write(`You are currently in ${process.env.currentDir}\n`);
+  
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
+    prompt: '-> '
   });
+  
 
   rl.on('line', function (line) {
     const command = line.split(' ');
-    run(command, {userName, rl});
+    run(command, { userName, rl });
+    process.stdout.write(`You are currently in ${process.env.currentDir}\n`);
+    
   })
-  
+
   rl.on('SIGINT', () => {
     exitMessage(userName);
     rl.close();
